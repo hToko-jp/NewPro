f117eed7a59393dfeb31518c4ef353d45c77b728
hToko-jp <tokoro.h@geducation.memuro.net>
2026-01-06 10:39:25 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
-// Firebase Configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyDhePsbzObaE91wzG0cKNhDpXm1xmdK_o4",
-    authDomain: "fir-pro-babfd.firebaseapp.com",
-    projectId: "fir-pro-babfd",
-    storageBucket: "fir-pro-babfd.firebasestorage.app",
-    messagingSenderId: "517736474622",
-    appId: "1:517736474622:web:a6702bbf7007a591f8c2af",
-    measurementId: "G-83309CP6R5",
-    databaseURL: "https://fir-pro-babfd-default-rtdb.firebaseio.com/"
-};
-
-// Initialize Firebase (Compat version uses global 'firebase' object)
-if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-}
+// Initialize Database Reference
+// (Firebase is already initialized in firebase-config.js)
 const db = firebase.database();
 
 class Game {
